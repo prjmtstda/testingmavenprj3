f6774197db7ff64767b906fb897bbe5b26acde0f
QCT <snkta2018@gmail.com>
2022-06-23 10:59:16 +0530

    Commit

---
+++ src/test/java/pkg/Manager1.java
@@ -0,0 +1,15 @@
+package pkg;
+
+public class Manager1 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Java");
+		System.out.println("Oracle");
+		System.out.println("MySQL");
+		System.out.println("Selenium");
+		System.out.println("TestNG");
+		
+	}
+}
+
