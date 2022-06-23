ac24a23e1dfd131f0899d959ae130314d6597be4
prjmtstda <105961423+prjmtstda@users.noreply.github.com>
2022-06-23 12:06:21 +0530

    Commit

--- src/test/java/pkg/Manager1.java
+++ src/test/java/pkg/Manager1.java
@@ -6,6 +6,8 @@ public class Manager1
 	{
 		System.out.println("Testing");
 		System.out.println("QC Testing");
+		System.out.println("Commit");
+		System.out.println("Code Commit");
 		System.out.println("Done");
 	}
 }
