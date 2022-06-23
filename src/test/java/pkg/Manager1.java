b2b89256c1dab9583ec1bda9c976f4186f704469
QCT <snkta2018@gmail.com>
2022-06-23 11:36:01 +0530

    Commit

--- src/test/java/pkg/Manager1.java
+++ src/test/java/pkg/Manager1.java
@@ -4,14 +4,8 @@ public class Manager1
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Java");
-		System.out.println("Oracle");
-		System.out.println("MySQL");
-		System.out.println("Selenium");
-		System.out.println("TestNG");
-		System.out.println("Cucumber");
-		System.out.println("Jenkins");
-        System.out.println("JSON");
+		System.out.println("Testing");
+		System.out.println("QC Testing");
+		System.out.println("Done");
 	}
 }
-
