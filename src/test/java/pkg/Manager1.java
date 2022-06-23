0245db37ad7d2a7bc5e6054de18347a3389197a5
prjmtstda <105961423+prjmtstda@users.noreply.github.com>
2022-06-23 13:32:06 +0530

    Commit

--- src/test/java/pkg/Manager1.java
+++ src/test/java/pkg/Manager1.java
@@ -9,6 +9,7 @@ public class Manager1
 		System.out.println("Commit");
 		System.out.println("Code Commit");
 		System.out.println("Code Committed Successfully");
+		System.out.println("Code Committed Successfully - b2");
 		System.out.println("Done");
 	}
 }
